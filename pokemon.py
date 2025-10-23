ac828a88ec53e759a0c4d58ce7b8154abf831615
Camilo <camo2391@gmail.com>
2025-10-23 17:18:11 -0500

    Implemented dual typing in class pokemon

--- pokemon.py
+++ pokemon.py
@@ -1,21 +1,28 @@
 class pokemon:
-    def __init__(self, name, type):
+    def __init__(self, name, typing):
         self.name = name
-        self.type = type
-    
+        self.typing = typing
+ 
     def printID(self):
-        return f"My name is {self.name} and my type is {self.type}"
-    
+        if len(self.typing) == 1:
+            return f"My name is {self.name} and my type is {self.typing[0]}!"
+        elif len(self.typing) == 2:
+            return f"My name is {self.name} and my types are {self.typing[0]} and {self.typing[1]}!"
+        else:
+            return f"Error in typing definition"
+
 def battle(pokemon1, pokemon2):
     return f"This is the first battle ever between {pokemon1.name} and {pokemon2.name}. Who will win?!"
 
 
 def main():
-    pikachu = pokemon('Pikachu','Electric')
-    onyx = pokemon('Onix','Rock')
+    pikachu = pokemon('Pikachu',('Electric',))
+    onyx = pokemon('Onix',('Rock',))
+    gengar = pokemon('Gengar',('Ghost','Poison'))
 
     print(pikachu.printID())
     print(onyx.printID())
+    print(gengar.printID())
 
     print(battle(pikachu,onyx))
 
