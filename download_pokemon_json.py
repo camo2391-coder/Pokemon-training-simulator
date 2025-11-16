4f0dfc4468b520d457ebb6b513944989db22675b
Camilo <camo2391@gmail.com>
2025-11-16 14:33:37 -0500

    se modificó download_pokemon_json.py para que descarge los archivos json de una lista de pokemon en vez de 1 solo pokemon"

--- download_pokemon_json.py
+++ download_pokemon_json.py
@@ -3,15 +3,19 @@ import requests
 import json
 
 BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
-usr_input = input("Enter the name of the Pokemon (in case the pokemon name has spaces, use hyphens e.g. mr-mime):")
+#usr_input = input("Enter the name of the Pokemon (in case the pokemon name has spaces, use hyphens e.g. mr-mime):")
 
-try:
-    response = requests.get(BASE_URL + usr_input.lower())
-    response.raise_for_status()
-    data = response.json()
-except requests.exceptions.RequestException as e:
-    print(f"Error fetching Pokémon: {e}")
-    exit()
+poke_list = range(1,11) #list of first 10 pokemon IDs
 
-with open(usr_input.lower() + ".json", "w", encoding="utf-8") as archivo:
-    json.dump(response.json(), archivo, indent=2)
+for i in poke_list:
+    try:
+        response = requests.get(BASE_URL + str(i))
+        response.raise_for_status()
+        data = response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching Pokémon: {e}")
+        exit()
+
+    with open(data['name'] + ".json", "w", encoding="utf-8") as archivo:
+        json.dump(response.json(), archivo, indent=2)
+    print(f"Downloaded data for Pokémon ID: {i}, name: {data['name']}")
