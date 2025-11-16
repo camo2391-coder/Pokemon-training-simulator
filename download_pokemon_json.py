ad9a6c15c7bff1ac2567cc7e55ada30fb379ff6d
Camilo <camo2391@gmail.com>
2025-11-16 14:44:00 -0500

    se modificó para que el nombre del archivo json descargado incluya el id del pokemon

--- download_pokemon_json.py
+++ download_pokemon_json.py
@@ -5,7 +5,7 @@ import json
 BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
 #usr_input = input("Enter the name of the Pokemon (in case the pokemon name has spaces, use hyphens e.g. mr-mime):")
 
-poke_list = range(1,11) #list of first 10 pokemon IDs
+poke_list = range(1,4) #list of first 10 pokemon IDs
 
 for i in poke_list:
     try:
@@ -16,6 +16,6 @@ for i in poke_list:
         print(f"Error fetching Pokémon: {e}")
         exit()
 
-    with open(data['name'] + ".json", "w", encoding="utf-8") as archivo:
+    with open(str(i)+"_"+data['name'] + ".json", "w", encoding="utf-8") as archivo:
         json.dump(response.json(), archivo, indent=2)
     print(f"Downloaded data for Pokémon ID: {i}, name: {data['name']}")
